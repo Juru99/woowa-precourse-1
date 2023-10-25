f92cf7751db4f9a1708288b29e6fb17374d311f7
stegano9744 <stegano9744@gmail.com>
2023-10-25 23:16:39 +0900

    🔒 security: 객체 불변성 유지 및 보호
    파일 경로: src/constants/constants.js
    Object.freeze()를 사용해 코드 예측 가능성을 높이고 테스트와 디버깅 시 예상치 않은 부작용을 방지했습니다.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 // 콘솔 출력 상수 데이터
-export const PROMPT = {
+export const PROMPT = Object.freeze({
   startGame: '숫자 야구 게임을 시작합니다.',
   inputNumber: '숫자를 입력해주세요 : ',
   error: '[ERROR]',
@@ -8,4 +8,4 @@ export const PROMPT = {
   nothing: '낫싱',
   endGame: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
   restartOrExit: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
-};
+});
